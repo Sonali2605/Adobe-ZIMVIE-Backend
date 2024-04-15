ecdbc2d6a3c68faf9835b89f730fcabdf1f40b49
Sonali Dhiman <Sonali.Dhiman@harbingergroup.com>
2024-04-15 16:00:01 +0530

    author dashboard

--- db.js
+++ db.js
@@ -1,41 +1,47 @@
 const mongoose = require("mongoose");
 //mongodb+srv://sonali:<password>@cluster0.ttvbing.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
-mongoose.connect("mongodb+srv://sonali:sonali@cluster0.ttvbing.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
-const courseSchema= mongoose.Schema({
-    user_id: Number,
-    before_you_begin:String,
-    fullname: String,
-    email: String,
-    ldtype:String,
-    specification: String,
-    title: String,
-    date: Date,
-    totalhours: String
-})
+                 //mongodb+srv://sonali:sonali@cluster0.ttvbing.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
+mongoose.connect("mongodb+srv://sonali:sonali1234@cluster0.ij3nnwp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+// const courseSchema= mongoose.Schema({
+//     user_id: Number,
+//     before_you_begin:String,
+//     fullname: String,
+//     email: String,
+//     ldtype:String,
+//     specification: String,
+//     title: String,
+//     date: Date,
+//     totalhours: String
+// })
 
-const tokenSchema = mongoose.Schema({
-    access_token: String,
-    refresh_token: String,
-    expires_in: Date
-});
+// const tokenSchema = mongoose.Schema({
+//     access_token: String,
+//     refresh_token: String,
+//     expires_in: Date
+// });
 
-const courseQuestionSchema =  mongoose.Schema({
+const courseQuestionSchema = mongoose.Schema({
   courseId: {
-    type: String, // Assuming the course ID is a string
-    required: true
+    type: String,
+    required: true,
+    unique: true
   },
   questions: [{
-    question: { type: String, required: true },
+    text: {
+      type: String,
+      required: true
+    },
     options: {
-      true: { type: String, required: true },
-      false: { type: String, required: true }
+      type: [String],
+      enum: ['true', 'false'],
+      required: true
     }
   }]
 });
 
 
-const course = mongoose.model('courses', courseSchema);
-const token = mongoose.model('tokens', tokenSchema);
+// const course = mongoose.model('courses', courseSchema);
+// const token = mongoose.model('tokens', tokenSchema);
 const course_question = mongoose.model('course_question', courseQuestionSchema);
 
-module.exports = {course, token, course_question}
+module.exports = {course_question}
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const { createCourse } = require("./types");
-const { course, token } = require("./db");
+const {course_question } = require("./db");
 const axios = require("axios")
 const app = express();
 const corsOptions = {
@@ -22,221 +22,268 @@ const _dirname = path.dirname("");
 const buildpath = path.join(_dirname,"../Adobe-CPD-Frontend/dist");
 app.use(express.static(buildpath));
 
-app.get('/userReport',async(req,res) =>{
-    const {user_id} = req.query;
-    const courses = await course.find({user_id: user_id});
-    const user = {
-        user_id: user_id,
-        fullname: courses.length > 0 ? courses[0].fullname : "",
-        email: courses.length > 0 ? courses[0].email : ""
-    };
-    const courseData = courses.map(course => ({
-        before_you_begin: course.before_you_begin,
-        ldtype: course.ldtype,
-        specification: course.specification,
-        title: course.title,
-        date: course.date,
-        totalhours: course.totalhours
-    }));
-    res.json({ user: user, courses: courseData });
-})
 
-app.get('/adminReport', async (req, res) => {
-    const coursesByUser = await course.aggregate([
-        {
-            $group: {
-                _id: '$user_id',
-                fullname: { $first: '$fullname' }, // Common firstname for the user
-                email: { $first: '$email' },   // Common lastname for the user
-                courses: {
-                    $push: {
-                        before_you_begin: '$before_you_begin',
-                        ldtype: '$ldtype',
-                        specification: '$specification',
-                        title: '$title',
-                        date: '$date',
-                        totalhours: '$totalhours'
-                    }
-                }
-            }
+app.post('/createCourseQuestions', async (req, res) => {
+    try {
+        const { courseId, questions } = req.body;
+
+        // Check if a course with the given courseId already exists
+        const existingCourse = await course_question.findOne({ courseId });
+
+        if (existingCourse) {
+            existingCourse.questions = questions;
+            await existingCourse.save();
+            res.status(200).json({ message: 'Questions updated successfully', course: existingCourse });
+        } else {
+            // If course does not exist, create a new course with the provided questions
+            const newCourse = new course_question({ courseId, questions });
+            await newCourse.save();
+            res.status(201).json({ message: 'New course created with questions', course: newCourse });
         }
-    ]);
- // Array to store results of API calls for each user
-//  const reports = [];
-// //  const token = await getToken();
-//  // Iterate over each user's courses
-//  for (const user of coursesByUser) {
-//      const userId = user._id; // Assuming user_id is stored in _id field
-//      // Make API call to Adobe API for each user ID
-//      const userDataResponse = await axios.get(`${base_adobe_url}/primeapi/v2/users/${userId}/enrollments?include=learningObject&page[limit]=10&sort=dateEnrolled`,  {
-//         headers: {
-//             Authorization: `oauth ${token}`
-//         }
-//         });
-//     let almDuration = 0;
-//     if (userDataResponse && userDataResponse.data && userDataResponse.data.data) {
-//         // Filter learning objects with state "COMPLETED"
-//         const completedLearningObjects = userDataResponse.data.data.filter(item => item.attributes.state === "COMPLETED");
-//         // Iterate over completed learning objects
-//         completedLearningObjects.forEach(completedObject => {
-//             // Extract ID of completed learning object
-//             const completedObjectId = completedObject.id.split('_')[0];
-//             // Search for the completed object in included array
-//             const includedObject = userDataResponse.data.included.find(item => item.id === completedObjectId);
-    
-//             // If included object with completedObjectId is found
-//             if (includedObject) {
-//                 // Extract duration if available
-//                 const durationHours = includedObject.attributes.duration / 3600;
-//                 almDuration += durationHours;
-//             }
-//     })
-//     }
-//     user.almCourseDuration = almDuration;
-
-//      // Push the user data to reports array
-//      reports.push(user);
-//  }
-//  // Send the reports as response
-//  res.json(reports)
-res.json(coursesByUser)
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
 });
 
-async function getToken() {
+
+  app.get('/getQuestionsByCourseId', async (req, res) => {
     try {
-        // Find any existing token
-        let existingToken = await token.findOne();
-        if (!existingToken) {
-            // If no token exists, create a new token document in the database
-            existingToken = await createToken();
-        } else{
-            // Check if the existing token is expired
-            const check = await checkToken(existingToken.access_token);
-            if (existingToken.expires_in < new Date() || check.error ) {
-                // If expired, refresh the token
-                const refreshedToken = await refreshAccessToken(existingToken.refresh_token);
-                // Update the token document with the new access token and refresh token
-                existingToken.access_token = refreshedToken.access_token;
-                existingToken.refresh_token = refreshedToken.refresh_token;
-                existingToken.expires_in = calculateExpiryTime(refreshedToken.expires_in);
-                await existingToken.save();
-            }
+        const { courseId } = req.query;
+
+        // Find the course with the given course ID
+        const courses = await course_question.find({ courseId });
+console.log("11111111111111111111111111", courseId, courses)
+        // If no course is found, return 404
+        if (courses.length === 0) {
+            return res.status(404).json({ error: 'Course not found' });
         }
-        // Return the access token
-        return existingToken.access_token;
+
+        // Since courseId is unique, we can assume there's only one course found
+        const course = courses[0];
+
+        // Return the questions
+        res.json({ questions: course.questions });
     } catch (error) {
-        console.error('Error fetching or refreshing token:');
-         throw new Error('Failed to fetch or refresh token');
+        res.status(500).json({ error: error.message });
     }
-}
-
-async function checkToken(token) {
-    try {
-        const url =  `${base_adobe_url}/oauth/token/check?access_token=${token}`;
-        const response = await axios.get(
-            `${url}`
-        );
-        // Extract the refresh token from the response
-        const tokenData = response.data;
-        // Create a new token document in the database
-        return tokenData;
-    } catch (error) {
-        console.error('Error creating token:', error.response.data);
-        throw new Error('Failed to create token');
-    }
-}
+});
 
 
-async function createToken() {
-    try {
-        const client_id = clientId;
-        const client_secret = clientSecret;
-        const refresh_token = '371390c98a8c3a4a24922adf11fb6b08';
+// app.get('/userReport',async(req,res) =>{
+//     const {user_id} = req.query;
+//     const courses = await course.find({user_id: user_id});
+//     const user = {
+//         user_id: user_id,
+//         fullname: courses.length > 0 ? courses[0].fullname : "",
+//         email: courses.length > 0 ? courses[0].email : ""
+//     };
+//     const courseData = courses.map(course => ({
+//         before_you_begin: course.before_you_begin,
+//         ldtype: course.ldtype,
+//         specification: course.specification,
+//         title: course.title,
+//         date: course.date,
+//         totalhours: course.totalhours
+//     }));
+//     res.json({ user: user, courses: courseData });
+// })
 
-        const params = new URLSearchParams({
-            client_id,
-            client_secret,
-            refresh_token
-        });
-        const url =  `${base_adobe_url}/oauth/token/refresh`;
-        const response = await axios.post(
-            `${url}`,
-            params,
-            {
-                headers: {
-                    "Content-Type": "application/x-www-form-urlencoded",
-                },
-            }
-        );
-        // Extract the refresh token from the response
-        const tokenData = response.data;
-        // Create a new token document in the database
-        if(tokenData){
-             await token.create({
-                access_token: tokenData.access_token,
-                refresh_token: tokenData.refresh_token,
-                expires_in: calculateExpiryTime(tokenData.expires_in)
-            });
-        }
-        return tokenData;
-    } catch (error) {
-        console.error('Error creating token:', error.response.data);
-        throw new Error('Failed to create token');
-    }
-}
+// app.get('/adminReport', async (req, res) => {
+//     const coursesByUser = await course.aggregate([
+//         {
+//             $group: {
+//                 _id: '$user_id',
+//                 fullname: { $first: '$fullname' }, // Common firstname for the user
+//                 email: { $first: '$email' },   // Common lastname for the user
+//                 courses: {
+//                     $push: {
+//                         before_you_begin: '$before_you_begin',
+//                         ldtype: '$ldtype',
+//                         specification: '$specification',
+//                         title: '$title',
+//                         date: '$date',
+//                         totalhours: '$totalhours'
+//                     }
+//                 }
+//             }
+//         }
+//     ]);
+//  // Array to store results of API calls for each user
+// //  const reports = [];
+// // //  const token = await getToken();
+// //  // Iterate over each user's courses
+// //  for (const user of coursesByUser) {
+// //      const userId = user._id; // Assuming user_id is stored in _id field
+// //      // Make API call to Adobe API for each user ID
+// //      const userDataResponse = await axios.get(`${base_adobe_url}/primeapi/v2/users/${userId}/enrollments?include=learningObject&page[limit]=10&sort=dateEnrolled`,  {
+// //         headers: {
+// //             Authorization: `oauth ${token}`
+// //         }
+// //         });
+// //     let almDuration = 0;
+// //     if (userDataResponse && userDataResponse.data && userDataResponse.data.data) {
+// //         // Filter learning objects with state "COMPLETED"
+// //         const completedLearningObjects = userDataResponse.data.data.filter(item => item.attributes.state === "COMPLETED");
+// //         // Iterate over completed learning objects
+// //         completedLearningObjects.forEach(completedObject => {
+// //             // Extract ID of completed learning object
+// //             const completedObjectId = completedObject.id.split('_')[0];
+// //             // Search for the completed object in included array
+// //             const includedObject = userDataResponse.data.included.find(item => item.id === completedObjectId);
+    
+// //             // If included object with completedObjectId is found
+// //             if (includedObject) {
+// //                 // Extract duration if available
+// //                 const durationHours = includedObject.attributes.duration / 3600;
+// //                 almDuration += durationHours;
+// //             }
+// //     })
+// //     }
+// //     user.almCourseDuration = almDuration;
+
+// //      // Push the user data to reports array
+// //      reports.push(user);
+// //  }
+// //  // Send the reports as response
+// //  res.json(reports)
+// res.json(coursesByUser)
+// });
+
+// async function getToken() {
+//     try {
+//         // Find any existing token
+//         let existingToken = await token.findOne();
+//         if (!existingToken) {
+//             // If no token exists, create a new token document in the database
+//             existingToken = await createToken();
+//         } else{
+//             // Check if the existing token is expired
+//             const check = await checkToken(existingToken.access_token);
+//             if (existingToken.expires_in < new Date() || check.error ) {
+//                 // If expired, refresh the token
+//                 const refreshedToken = await refreshAccessToken(existingToken.refresh_token);
+//                 // Update the token document with the new access token and refresh token
+//                 existingToken.access_token = refreshedToken.access_token;
+//                 existingToken.refresh_token = refreshedToken.refresh_token;
+//                 existingToken.expires_in = calculateExpiryTime(refreshedToken.expires_in);
+//                 await existingToken.save();
+//             }
+//         }
+//         // Return the access token
+//         return existingToken.access_token;
+//     } catch (error) {
+//         console.error('Error fetching or refreshing token:');
+//          throw new Error('Failed to fetch or refresh token');
+//     }
+// }
+
+// async function checkToken(token) {
+//     try {
+//         const url =  `${base_adobe_url}/oauth/token/check?access_token=${token}`;
+//         const response = await axios.get(
+//             `${url}`
+//         );
+//         // Extract the refresh token from the response
+//         const tokenData = response.data;
+//         // Create a new token document in the database
+//         return tokenData;
+//     } catch (error) {
+//         console.error('Error creating token:', error.response.data);
+//         throw new Error('Failed to create token');
+//     }
+// }
 
 
-async function refreshAccessToken(refreshToken) {
-    try {
-        const params = new URLSearchParams({
-            client_id : clientId,
-            client_secret : clientSecret,
-            refresh_token: refreshToken
-        });
+// async function createToken() {
+//     try {
+//         const client_id = clientId;
+//         const client_secret = clientSecret;
+//         const refresh_token = '371390c98a8c3a4a24922adf11fb6b08';
 
-        const response = await axios.post(`${base_adobe_url}/oauth/token/refresh`, params, {
-            headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
-        });
+//         const params = new URLSearchParams({
+//             client_id,
+//             client_secret,
+//             refresh_token
+//         });
+//         const url =  `${base_adobe_url}/oauth/token/refresh`;
+//         const response = await axios.post(
+//             `${url}`,
+//             params,
+//             {
+//                 headers: {
+//                     "Content-Type": "application/x-www-form-urlencoded",
+//                 },
+//             }
+//         );
+//         // Extract the refresh token from the response
+//         const tokenData = response.data;
+//         // Create a new token document in the database
+//         if(tokenData){
+//              await token.create({
+//                 access_token: tokenData.access_token,
+//                 refresh_token: tokenData.refresh_token,
+//                 expires_in: calculateExpiryTime(tokenData.expires_in)
+//             });
+//         }
+//         return tokenData;
+//     } catch (error) {
+//         console.error('Error creating token:', error.response.data);
+//         throw new Error('Failed to create token');
+//     }
+// }
 
-        return response.data;
-    } catch (error) {
-        console.error('Error refreshing token:', error.response.data);
-        throw new Error('Failed to refresh token');
-    }
-}
 
-// Function to calculate expiry time based on expiresIn value
-function calculateExpiryTime(expiresIn) {
-    const now = new Date();
-    now.setSeconds(now.getSeconds() + expiresIn);
-    return now;
-}
+// async function refreshAccessToken(refreshToken) {
+//     try {
+//         const params = new URLSearchParams({
+//             client_id : clientId,
+//             client_secret : clientSecret,
+//             refresh_token: refreshToken
+//         });
+
+//         const response = await axios.post(`${base_adobe_url}/oauth/token/refresh`, params, {
+//             headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
+//         });
+
+//         return response.data;
+//     } catch (error) {
+//         console.error('Error refreshing token:', error.response.data);
+//         throw new Error('Failed to refresh token');
+//     }
+// }
+
+// // Function to calculate expiry time based on expiresIn value
+// function calculateExpiryTime(expiresIn) {
+//     const now = new Date();
+//     now.setSeconds(now.getSeconds() + expiresIn);
+//     return now;
+// }
 
 
-app.post('/cpdData',async (req,res) =>{
-    const createPayload = req.body;
-    const parsePayload= createCourse.safeParse(createPayload);
-    if(!parsePayload.success){
-        res.status(411).json({
-            msg:"You send wrong inputs"
-        })
-        return;
-    }
-    await course.create({
-        user_id: createPayload.user_id,
-        before_you_begin:createPayload.before_you_begin,
-        fullname: createPayload.fullname,
-        email: createPayload.email,
-        ldtype:createPayload.ldtype,
-        specification: createPayload.specification,
-        title: createPayload.title,
-        date: createPayload.date,
-        totalhours: createPayload.totalhours
-    })
-    res.json({
-        msg: "Course created"
-    })
-})
+// app.post('/cpdData',async (req,res) =>{
+//     const createPayload = req.body;
+//     const parsePayload= createCourse.safeParse(createPayload);
+//     if(!parsePayload.success){
+//         res.status(411).json({
+//             msg:"You send wrong inputs"
+//         })
+//         return;
+//     }
+//     await course.create({
+//         user_id: createPayload.user_id,
+//         before_you_begin:createPayload.before_you_begin,
+//         fullname: createPayload.fullname,
+//         email: createPayload.email,
+//         ldtype:createPayload.ldtype,
+//         specification: createPayload.specification,
+//         title: createPayload.title,
+//         date: createPayload.date,
+//         totalhours: createPayload.totalhours
+//     })
+//     res.json({
+//         msg: "Course created"
+//     })
+// })
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'dist', 'index.html'));
   });
